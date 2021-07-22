0d8b1d2e61fdfbcbba28b5aea4061997f27b13ed
Adam15Free <33614133+Adam15Free@users.noreply.github.com>
2021-07-22 10:50:23 -0400

    initial

---
+++ app/src/main/java/com/dwarfcraft/bill_data/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.dwarfcraft.bill_data.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
